88ca1d00bf45bae5e53cb306638dce5ec4fa14c0
Sheldon Juncker <sheldonjuncker@gmail.com>
2019-10-03 23:29:22 -0400

    Adding more saving and data definitios.

---
+++ app/Storm/DataDefinition/DataFieldDefinition.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Storm\DataDefinition;
+
+/**
+ * Class DataField
+ *
+ * Represents a data field to be used on a Model.
+ *
+ * @package App\Storm\DataDefinition
+ */
+class DataField
+{
+
+}
---
+++ app/Storm/Saver/SqlSaver.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Storm\Saver;
+
+
+class SqlSaver
+{
+
+}
