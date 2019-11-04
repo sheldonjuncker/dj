8c93e77c7f8e413ab2aac417c4b287d4cedbc45f
Sheldon Juncker <sheldonjuncker@gmail.com>
2019-11-03 20:21:21 -0500

    Improved searching.

--- da/venv/jung.py
+++ da/venv/jung.py
@@ -9,27 +9,41 @@ class Jung:
         search_terms = []
         for token in tokens:
             search_terms.append(token[1])
+
+        # Build unions for query to get all dreams containing a search term
         sql = """
-            (
-                SELECT
-                    bin_to_uuid(dream_id) AS 'dream_id'
-                FROM
-                    freud.dream_word_freq dwf
-                INNER JOIN
-                    freud.word ON(
-                        word.id = dwf.word_id
-                    )
-                WHERE
-                    word.search LIKE %s
+    (
+        SELECT
+           dream_id, dwf.word_id, dwf.frequency
+        FROM
+            freud.dream_word_freq dwf
+        INNER JOIN
+            freud.word ON(
+                word.id = dwf.word_id
             )
-        """
+        WHERE
+            word.search LIKE %s
+    )"""
         first_term = search_terms.pop()
         params = [first_term + "%"]
 
-
         for search_term in search_terms:
-            sql += "\nunion\n" + sql
+            sql += "\n\n\tunion\n" + sql
             params.append(search_term + "%")
+
+        # Add in the main query to work on unioned data
+        sql = "(" + sql + "\n)";
+        sql = """
+SELECT
+    bin_to_uuid(z.dream_id) AS dream_id,
+    SUM(z.frequency) AS total_frequency
+FROM
+""" + sql + """ z
+GROUP BY
+    dream_id
+ORDER BY
+    total_frequency DESC
+        """
 
         print(sql)
 
